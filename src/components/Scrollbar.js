2e6bf0f390b476857f90de67d9c111b88868f23e
eloquence98 <ibneenayat1998@gmail.com>
2023-07-23 13:05:45 +0500

    set scrollbar max height

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -1,18 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
+// import { useEffect, useRef, useState } from "react";
 
 function Scrollbar({ children }) {
-  const [height, setHeight] = useState(null)
-  const ref = useRef(null)
+  // const [height, setHeight] = useState(null)
+  // const ref = useRef(null)
 
-  // height set
-  useEffect(() => {
-    setHeight(ref.current.clientHeight)
-  }, [])
+  // // height set
+  // useEffect(() => {
+  //   setHeight(ref.current.clientHeight)
+  // }, [])
   return (
     <div
       className={'scroll-bar'}
-      style={{ maxHeight: `${height - 17.5}px` }}
-      ref={ref}
+      // style={{ maxHeight: `${height - 17.5}px` }}
+      style={{ maxHeight: `23.1rem` }}
+      // ref={ref}
     >
       {children}
     </div>
